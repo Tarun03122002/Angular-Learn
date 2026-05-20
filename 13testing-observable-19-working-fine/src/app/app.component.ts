5242dac0f5c8ce54acc0cbf1c52ae548331a89ba
Tarun Kumar <91051982+Tarun0312@users.noreply.github.com>
2026-05-20 07:24:58 +0000

    of and from from rxjs operators to convert into observable

--- 13testing-observable-19-working-fine/src/app/app.component.ts
+++ 13testing-observable-19-working-fine/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Observable } from 'rxjs';
+import { from, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -20,14 +20,27 @@ dataList :any[]= []
   // })
 
   //creating stream of data
-  testObs = new Observable((observer)=> {
-    setTimeout(()=> {observer.next(1)},1000);
-    setTimeout(()=> {observer.next(2)},2000);
-    setTimeout(()=> {observer.next(3)},3000);
-    setTimeout(()=> {observer.next(4)},4000);
-    setTimeout(()=> {observer.next(5)},5000);
-  })
+  // testObs = new Observable((observer)=> {
+  //   setTimeout(()=> {observer.next(1)},1000);
+  //   setTimeout(()=> {observer.next(2)},2000);
+  //   setTimeout(()=> {observer.next(3)},3000);
+  //   setTimeout(()=> {observer.next(4)},4000);
+  //   setTimeout(()=> {observer.next(5)},5000);
+  // })
 
+
+
+  // using of operator
+  newArr = [1,2,3,4,5]
+  newArr2 = ['a','b','c','d']
+
+  // testObs = of(this.newArr,this.newArr2,true,'null') 
+  // testObs = of(this.newArr2) 
+
+
+  // using from operator
+  // testObs = from(this.newArr) //emit value in iterable one by one
+ testObs = from('89001211')
 
 
   // 2) Subscribing an observable
@@ -35,7 +48,7 @@ dataList :any[]= []
   onClick(){
     this.toggle = !this.toggle
     this.testObs.subscribe((data : any) => {
-      console.log(data,this.dataList);
+      console.log(data);
       
       this.dataList = [...this.dataList,data]
        });
