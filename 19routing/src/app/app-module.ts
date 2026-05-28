e4bdf6ea9eff3d279b0f97c47266185554c8b829
Tarun Kumar <91051982+Tarun0312@users.noreply.github.com>
2026-05-28 05:10:26 +0000

    creating a routing module and move all routes from app module to routing-module.ts

--- 19routing/src/app/app-module.ts
+++ 19routing/src/app/app-module.ts
@@ -7,47 +7,46 @@ import { Footer } from './footer/footer';
 import { Courses } from './courses/courses';
 import { Contact } from './contact/contact';
 import { CourseService } from './Services/course.service';
-import { RouterModule, Routes } from '@angular/router';
 import { About } from './about/about';
 import { NotFound } from './not-found/not-found';
 import { TestimonyComponent } from './home/testimony/testimony.component';
 import { ServicesComponent } from './home/services/services.component';
 import { PopularComponent } from './home/popular/popular.component';
 import { ContactUsComponent } from './home/contact-us/contact-us.component';
-import { CurrencyPipe } from '@angular/common';
 import { BannerComponent } from './home/banner/banner.component';
-import { UserService } from './Services/user.service';
 import { ServicesService } from './Services/service.service';
 import { CourseDetailComponent } from './course-detail/course-detail.component';
+import { RoutingModule } from './routing-module';
 
 // Defining a route
 // 1) Use Routes Array to specify route in each object
 
-const routes: Routes = [
-  // { path: '**', component: NotFound },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '', component: Home },
-  { path: 'home', component: Home },
-  { path: 'courses', component: Courses },
-  { path: 'contact', component: Contact },
-  { path: 'about', component: About },
+// Moving routes to routing module
+// const routes: Routes = [
+//   // { path: '**', component: NotFound },
+//   { path: '', redirectTo: 'home', pathMatch: 'full' },
+//   { path: '', component: Home },
+//   { path: 'home', component: Home },
+//   { path: 'courses', component: Courses },
+//   { path: 'contact', component: Contact },
+//   { path: 'about', component: About },
 
-  // // Route parameter are the dynamic part of a route whose value can change.This provide way to pass additional information to that route
-  // // use colon through which angular knows it is a dynamic route,variable which store additional information
-  // {path:'courses/course/:id',component:CourseDetailComponent},
+//   // // Route parameter are the dynamic part of a route whose value can change.This provide way to pass additional information to that route
+//   // // use colon through which angular knows it is a dynamic route,variable which store additional information
+//   // {path:'courses/course/:id',component:CourseDetailComponent},
 
-  // working with child routes
-  {
-    path: 'courses', children: [ //path: 'courses' tells us the parent is courses here and we are defined all the childs in children array
-      { path: 'course/:id', component: CourseDetailComponent },
-      { path: "popular-courses", component: PopularComponent }
-    ]
-  },
+//   // working with child routes
+//   {
+//     path: 'courses', children: [ //path: 'courses' tells us the parent is courses here and we are defined all the childs in children array
+//       { path: 'course/:id', component: CourseDetailComponent },
+//       { path: "popular-courses", component: PopularComponent }
+//     ]
+//   },
 
 
-  // page not found route, ** wildcard route matches for all routes .Must be last element in routes array
-  { path: '**', component: NotFound },
-];
+//   // page not found route, ** wildcard route matches for all routes .Must be last element in routes array
+//   { path: '**', component: NotFound },
+// ];
 //  2) REGISTER routes array to angular provider array using RoutingModule.forRoot(routes) to be avaible throughout the app
 // if used forChild ,it means it will be avaiblable for specify part of app
 
@@ -57,7 +56,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [App, Home, Header, Footer, Courses, Contact, About, NotFound, TestimonyComponent, ServicesComponent, PopularComponent, CourseDetailComponent, ContactUsComponent, BannerComponent],
 
-  imports: [BrowserModule, CurrencyPipe, RouterModule.forRoot(routes)],
+  imports: [BrowserModule,RoutingModule],
   providers: [provideBrowserGlobalErrorListeners(), CourseService, ServicesService],
 
   bootstrap: [App],
---
+++ 19routing/src/app/routing-module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from "@angular/core";
+import { CourseDetailComponent } from "./course-detail/course-detail.component";
+import { PopularComponent } from "./home/popular/popular.component";
+import { NotFound } from "./not-found/not-found";
+import { About } from "./about/about";
+import { Contact } from "./contact/contact";
+import { Courses } from "./courses/courses";
+import { Home } from "./home/home";
+import { RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '', component: Home },
+    { path: 'home', component: Home },
+    { path: 'courses', component: Courses },
+    { path: 'contact', component: Contact },
+    { path: 'about', component: About },
+    {
+        path: 'courses', children: [
+            { path: 'course/:id', component: CourseDetailComponent },
+            { path: "popular-courses", component: PopularComponent }
+        ]
+    },
+    { path: '**', component: NotFound },
+];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],//forRoot(routes) routes will avaliable throughout all the application
+    //  forChild(routes) -> routes will be avaiblble only for featured module
+    exports: [RouterModule] //export RouterModule so that it can be accessed in app Module
+})
+export class RoutingModule {
+
+}
