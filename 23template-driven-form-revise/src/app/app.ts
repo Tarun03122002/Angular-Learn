47415c90ce554d2804e2549511d1c440dbf7c70c
Tarun Kumar <91051982+Tarun0312@users.noreply.github.com>
2026-06-11 05:53:28 +0000

    Accessing form control value using value and controls property

--- 23template-driven-form-revise/src/app/app.ts
+++ 23template-driven-form-revise/src/app/app.ts
@@ -10,10 +10,18 @@ import { NgForm } from '@angular/forms';
 export class App {
   protected readonly title = signal('23template-driven-form-revise');
 
-  @ViewChild('registrationForm') form! : NgForm
-  onSubmit(){
+  @ViewChild('registrationForm') form!: NgForm
+  onSubmit() {
     // instead of receiving form object,we use View child
     console.log(this.form);
+
+    // Accessing form control value using value property
+    console.log(this.form.value.fName);
+    console.log(this.form.value.lName, this.form.value.dob)
+    // Accessing form control value using controls property
+    console.log(this.form.controls['streetAdd1'].value,this.form.controls['country'].value);
     
+
+
   }
 }
