6cab3e2014366727c8fa01706677b26b05605bf6
Tarun Kumar <91051982+Tarun0312@users.noreply.github.com>
2026-06-11 11:58:19 +0000

    grouping of form control->  Usecase if we want to show some error on group of form control instead of individual form control

--- 23template-driven-form-revise/src/app/app.ts
+++ 23template-driven-form-revise/src/app/app.ts
@@ -23,7 +23,9 @@ export class App {
     console.log(this.form.value.fName);
     console.log(this.form.value.lName, this.form.value.dob)
     // Accessing form control value using controls property
-    console.log(this.form.controls['streetAdd1'].value, this.form.controls['country'].value);
+    // console.log(this.form.controls['streetAdd1'].value, this.form.controls['country'].value);
+    console.log(this.form.value.address.streetAdd1,this.form.value.address.country);
+    
 
 
 
