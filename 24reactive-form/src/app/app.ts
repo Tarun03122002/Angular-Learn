fbed8388986df52bea34f3749808661a26f6cf38
Tarun Kumar <91051982+Tarun0312@users.noreply.github.com>
2026-06-13 17:01:06 +0000

    feat: add address form group with validation for street address, country, and postal code

--- 24reactive-form/src/app/app.ts
+++ 24reactive-form/src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { required } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-root',
@@ -22,23 +23,26 @@ export class App {
 
   createForm() {
     this.registrationForm = new FormGroup({
-      fName: new FormControl('',Validators.required),
-      lName: new FormControl('',Validators.required),
-      email: new FormControl('',[Validators.required,Validators.email]),
+      fName: new FormControl('', Validators.required),
+      lName: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
       userName: new FormControl(''),
       dob: new FormControl(''),
       gender: new FormControl('male'),
-      streetAdd: new FormControl(''),
-      country: new FormControl('India'),
-      city: new FormControl(''),
-      region: new FormControl(''),
-      postal: new FormControl('')
+      // Grouping of form control ,use  formGroupName directive at parent wrapper of all these controls
+      address: new FormGroup({
+        streetAdd: new FormControl('',Validators.required),
+        country: new FormControl('India',Validators.required),
+        city: new FormControl(''),
+        region: new FormControl(''),
+        postal: new FormControl('',Validators.required)
+      })
 
     })
   }
 
-  onSubmit(){
-    console.log("form",this.registrationForm);
-    
+  onSubmit() {
+    console.log("form", this.registrationForm);
+
   }
 }
