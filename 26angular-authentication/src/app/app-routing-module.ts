8b4f5772bac92a6a185e2b4b81f4e4977e18321c
Tarun Kumar <139613918+Tarun03122002@users.noreply.github.com>
2026-07-24 11:15:16 +0000

    Moving Login route to auth module

--- 26angular-authentication/src/app/app-routing-module.ts
+++ 26angular-authentication/src/app/app-routing-module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { canActivate } from './Guard/auth.guard';
-import { Overview } from './dashboard/overview/overview';
-import { Stats } from './dashboard/stats/stats';
 
 const routes: Routes = [
    { path: '', component: HomeComponent }, 
-    { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
--- 26angular-authentication/src/app/login/auth-module.ts
+++ 26angular-authentication/src/app/login/auth-module.ts
@@ -3,13 +3,19 @@
 import { NgModule } from "@angular/core";
 import { LoginComponent } from "./login.component";
 import { SharedModule } from "../shared.module";
+import { RouterModule, Routes } from "@angular/router";
 
+
+const routes : Routes = [
+    {path:'login',component:LoginComponent}
+]
 @NgModule({
     declarations:[
         LoginComponent
     ],
     imports:[
-        SharedModule
+        SharedModule,
+        RouterModule.forChild(routes)
     ]
 })
 export class AuthModule{
