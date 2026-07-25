3686ed141362db39a13f7615421828e7679e02eb
Tarun Kumar <139613918+Tarun03122002@users.noreply.github.com>
2026-07-25 06:55:22 +0000

    used  preloadingStrategy

--- 26angular-authentication/src/app/app-routing-module.ts
+++ 26angular-authentication/src/app/app-routing-module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 // Lazy Load Dashboard Module
@@ -10,8 +10,10 @@ const routes: Routes = [
   { path: "login", loadChildren: () => import('./login/auth-module').then((module) => module.AuthModule) }
 ];
 
+// Now we are loaded dashboard and AuthModule when we navigate to dashboard or login route,then so there it will take some time to load
+// Better if we Eagerload appmodule after that(thodi der baad) in background ,all lazy load modules are loading so use preloading strategy
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{preloadingStrategy:PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
