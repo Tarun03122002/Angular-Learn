09acb5a6440ddb29f072872a92922cb65d6c415b
Tarun Kumar <139613918+Tarun03122002@users.noreply.github.com>
2026-07-31 07:23:07 +0000

    providing service to standalone

--- 28standalone/src/main.ts
+++ 28standalone/src/main.ts
@@ -8,3 +8,8 @@ bootstrapApplication(App,{
 
 })
   .catch(err => console.error(err));
+// IN STANDALONE COMPONENTS,TWO WAY OF PROVIDING SERVICES SO THAT A SINGLE INSTANCE IS THERE
+// ONE IS PROVIDEDin : root
+// others is in bootstrap application -> 2nd argument {providers:[]}
+
+// if we are not providing services separetly ,then there will  be one instance
