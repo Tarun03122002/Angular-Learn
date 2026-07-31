fe01b001e6f846781b5ec547f6eeb6ba116cdfed
Tarun Kumar <139613918+Tarun03122002@users.noreply.github.com>
2026-07-31 07:13:57 +0000

    CONVERTING HOME INTO STANDALONE COMPONENT

--- 28standalone/src/app/home/home.ts
+++ 28standalone/src/app/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Detail } from './detail/detail';
 
 @Component({
   selector: 'app-home',
-  standalone: false,
+  standalone: true,
+  imports:[Detail],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
